a90f1dec187f65cd2537d3e65e658609abd498e3
Amir <amirhamza11793@gmail.com>
2024-04-08 16:32:11 -0700

    Homepage

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  // Method to scroll to the contact section
+  scrollToContact() {
+    const contactSection = document.getElementById('contact');
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+}
